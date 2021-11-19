1e3c716bf11357035242f6207223ba7834a156de
Prince kunzwa <princekunzwa@outlook.com>
2021-11-19 19:35:47 +0000

    Base Interface and Change Listener push

--- src/main/java/com/example/todolist/Controller.java
+++ src/main/java/com/example/todolist/Controller.java
@@ -1,10 +1,13 @@
 package com.example.todolist;
 
 import datamodel.TodoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -16,6 +19,12 @@ public class Controller {
     private List<TodoItem> todoItems;
     @FXML
     private ListView todoListView;
+
+    @FXML
+    private TextArea itemDetailsTextArea;
+
+    @FXML
+    private Label deadlineLabel;
 
     public void initialize() {
         TodoItem item1 = new TodoItem("Fix the sink",
@@ -41,15 +50,35 @@ public class Controller {
         todoItems.add(item4);
         todoItems.add(item5);
 
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends  TodoItem> observable,
+            TodoItem oldValue, TodoItem newValue) {
+                if (newValue != null) {
+                    TodoItem item = (TodoItem) todoListView.getSelectionModel().getSelectedItem();
+                    itemDetailsTextArea.setText(item.getDetails());
+                    }
+                }
+            });
+
         todoListView.getItems().setAll(todoItems);
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
 
     }
 
     @FXML
     public void handleClickListView(){
         TodoItem item = (TodoItem) todoListView.getSelectionModel().getSelectedItem();
+        itemDetailsTextArea.setText(item.getDetails());
+        deadlineLabel.setText(item.getDeadline().toString());
 //        System.out.println("The selected item is: " + item);
+//        StringBuilder sb = new StringBuilder(item.getDetails());
+//        sb.append("\n\n\n\n");
+//        sb.append("Due: ");
+//        sb.append(item.getDeadline().toString());
+//        itemDetailsTextArea.setText(sb.toString());
+
     }
 
 
